f448d7a286822ccb6ae4c8d66215212137a9ceb0
Edward7 <ediro174@gmail.com>
2022-07-29 20:11:57 +0300

    1

--- Nocturnal ESP/Config.cs
+++ Nocturnal ESP/Config.cs
@@ -30,8 +30,8 @@ namespace Nocturnal
             if (!Directory.Exists(Directory.GetCurrentDirectory() + "//Nocturnal"))
                 Directory.CreateDirectory(Directory.GetCurrentDirectory() + "//Nocturnal");
 
-            if (!File.Exists(Directory.GetCurrentDirectory() + "//Nocturnal//Config.Json"))
-                File.WriteAllText(Directory.GetCurrentDirectory() + "//Nocturnal//Config.Json", JsonConvert.SerializeObject(new Json()
+            if (!File.Exists(Directory.GetCurrentDirectory() + "//Nocturnal//ESP_Config.Json"))
+                File.WriteAllText(Directory.GetCurrentDirectory() + "//Nocturnal//ESP_Config.Json", JsonConvert.SerializeObject(new Json()
                 {
                     DefaultColor = new int[] { 255, 8, 90 },
                     FriendsColor = new int[] { 255, 251, 0 },
@@ -39,7 +39,7 @@ namespace Nocturnal
                     Width = 0.5f
                 }));
 
-            s_js = JsonConvert.DeserializeObject<Json>(File.ReadAllText(Directory.GetCurrentDirectory() + "//Nocturnal//Config.Json"));
+            s_js = JsonConvert.DeserializeObject<Json>(File.ReadAllText(Directory.GetCurrentDirectory() + "//Nocturnal//ESP_Config.Json"));
             s_defaultColor = new Color32(byte.Parse(s_js.DefaultColor[0].ToString()), byte.Parse(s_js.DefaultColor[1].ToString()), byte.Parse(s_js.DefaultColor[2].ToString()),255);
             s_friendsColor = new Color32(byte.Parse(s_js.FriendsColor[0].ToString()), byte.Parse(s_js.FriendsColor[1].ToString()), byte.Parse(s_js.FriendsColor[2].ToString()), 255);
          }
